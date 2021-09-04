784397bd5e7a98663a29713ad83be98278523eb2
rojo2530 <jcantosg@gmail.com>
2021-09-05 00:23:13 +0200

    Add movie interface api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import movieDB from '../api/movieDB';
+import { MovieDbNowPlaying } from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(res => console.log(res.data));
+    movieDB
+      .get<MovieDbNowPlaying>('/now_playing')
+      .then(res => console.log(res.data.results[0].title));
   }, []);
   return (
     <View>
